4983fd9d556d1b5e2487ce97e317bf76f863ca97
Rizza Carla Modina Marzo <rizzacarla.12@gmail.com>
2026-03-09 23:57:10 -0700

    Create Home component for the travel website

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Star Travel Agency</h1>
+            <p>Explore the universe with our exclusive travel packages!</p>
+        </div>
+    );
+};
+
+export default Home;
